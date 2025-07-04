c992e02528485c2a4298e64988f0e3f19cec0555
KidGameZ1 <kaydenjalexis@outlook.com>
2025-07-04 18:56:15 -0500

    Everything works!!!

---
+++ src/main/java/net/nightshade/divinity_engine/registry/DivinityEngineArgumentTypes.java
@@ -0,0 +1,28 @@
+package net.nightshade.divinity_engine.registry;
+
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.commands.synchronization.SingletonArgumentInfo;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nightshade.divinity_engine.DivinityEngineMod;
+import net.nightshade.divinity_engine.commands.arg.GodsArgument;
+
+public class DivinityEngineArgumentTypes {
+    private static final DeferredRegister<ArgumentTypeInfo<?, ?>> registry;
+    private static final RegistryObject<SingletonArgumentInfo<GodsArgument>> GODS_ARGUMENT_REGISTRY;
+
+    public DivinityEngineArgumentTypes() {
+    }
+
+    public static void init(IEventBus modEventBus) {
+        registry.register(modEventBus);
+    }
+
+    static {
+        registry = DeferredRegister.create(ForgeRegistries.COMMAND_ARGUMENT_TYPES, DivinityEngineMod.MODID);
+        GODS_ARGUMENT_REGISTRY = registry.register("gods_argument", () -> (SingletonArgumentInfo) ArgumentTypeInfos.registerByClass(GodsArgument.class, SingletonArgumentInfo.contextFree(GodsArgument::god)));
+    }
+}
